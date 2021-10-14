b277ec0c0f6fe221220a63bf96b21b142a3479bd
Vladislav Topchu <48028756+vladtopchu@users.noreply.github.com>
2021-10-14 17:18:56 +0300

    Added HelloActivity and FirebaseAuth library

--- app/src/main/java/com/topchu/recoverfrombreakup/RecoverFromBreakupApplication.kt
+++ app/src/main/java/com/topchu/recoverfrombreakup/RecoverFromBreakupApplication.kt
@@ -1,6 +1,17 @@
 package com.topchu.recoverfrombreakup
 
 import android.app.Application
+import android.content.Intent
+import com.topchu.recoverfrombreakup.ui.HelloActivity
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class RecoverFromBreakupApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val intent = Intent(this, HelloActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        startActivity(intent)
+    }
 }
--- app/src/main/java/com/topchu/recoverfrombreakup/ui/MainActivity.kt
+++ app/src/main/java/com/topchu/recoverfrombreakup/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.topchu.recoverfrombreakup
+package com.topchu.recoverfrombreakup.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.topchu.recoverfrombreakup.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
