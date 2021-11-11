d45fb4a77c8a0960ce92bf44a43429025ec0ac2d
Vladislav Topchu <48028756+vladtopchu@users.noreply.github.com>
2021-11-11 16:46:31 +0300

    fixed timeobject to show 24hr format instead of am/pm

--- app/src/main/java/com/topchu/recoverfrombreakup/utils/Extensions.kt
+++ app/src/main/java/com/topchu/recoverfrombreakup/utils/Extensions.kt
@@ -10,7 +10,7 @@ import java.util.*
 
 fun <T> MutableLiveData<T>.asLiveData() = this as LiveData<T>
 
-fun Long.toTimeString() = SimpleDateFormat("dd:MM:yyyy:hh:mm:ss", Locale.ROOT).format(Date(this))
+fun Long.toTimeString() = SimpleDateFormat("dd:MM:yyyy:HH:mm:ss", Locale.ROOT).format(Date(this))
 fun Long.toTimeObject(): TimeObject {
     val array = this.toTimeString().split(":").map { it.toInt() }
     return TimeObject(
